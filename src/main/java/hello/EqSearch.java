c7f94cdabee825990b22dcf74f98c642446a8dae
Benjamin Lee <benjamin_lee@ucsb.edu>
2019-11-20 20:43:07 -0800

    fixed merge issue

--- src/main/java/hello/EqSearch.java
+++ src/main/java/hello/EqSearch.java
@@ -1,42 +1,14 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 package hello;
-=======
->>>>>>> e8d7fcb43c9f795a03f326edb3c5760784d5c3a7
-=======
->>>>>>> 8c1f04d755101b82d223389c6f6ce8d0eb5fb357
 public class EqSearch {
 	private int distance;
 	private int minmag;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8c1f04d755101b82d223389c6f6ce8d0eb5fb357
 	public int getDistance(){
 		return this.distance;
 	}
 
 	public int getMinmag(){
 		return this.minmag;
-<<<<<<< HEAD
-=======
-	public EqSearch(){
-	}
-
-	public int getDistance(){
-		return distance;
-	}
-
-	public int getMinmag(){
-		return minmag;
->>>>>>> e8d7fcb43c9f795a03f326edb3c5760784d5c3a7
-=======
-  }
-  
-	public EqSearch(){
->>>>>>> 8c1f04d755101b82d223389c6f6ce8d0eb5fb357
-	}
 
 	public void setDistance(int dist){
 		this.distance = dist;
@@ -45,19 +17,7 @@ public class EqSearch {
 	public void setMinmag(int min){
 		this.minmag = min;
 	}
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 	public EqSearch()
 	{
 	}
 }
-=======
-}
-
-
-
->>>>>>> e8d7fcb43c9f795a03f326edb3c5760784d5c3a7
-=======
-}
->>>>>>> 8c1f04d755101b82d223389c6f6ce8d0eb5fb357
