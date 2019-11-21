5a1f61d458ceb070fc40c3833f13e3e52486c208
Benjamin Lee <benjamin_lee@ucsb.edu>
2019-11-20 20:37:01 -0800

    bl - adding webcontroller

---
+++ src/main/java/hello/EqSearch.java
@@ -0,0 +1,25 @@
+package hello;
+public class EqSearch {
+	private int distance;
+	private int minmag;
+
+	public int getDistance(){
+		return this.distance;
+	}
+
+	public int getMinmag(){
+		return this.minmag;
+	}
+
+	public void setDistance(int dist){
+		this.distance = dist;
+	}
+
+	public void setMinmag(int min){
+		this.minmag = min;
+	}
+
+	public EqSearch()
+	{
+	}
+}
