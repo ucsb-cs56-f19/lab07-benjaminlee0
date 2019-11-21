8fe3bf80c79f802dca9ecf6ac659c7543ae113ea
Benjamin Lee <benjamin_lee@ucsb.edu>
2019-11-20 17:32:48 -0800

    bl - mock dependencies for unit testing

---
+++ src/main/java/hello/EqSearch.java
@@ -0,0 +1,26 @@
+public class EqSearch {
+	private int distance;
+	private int minmag;
+
+	public EqSearch(){
+	}
+
+	public int getDistance(){
+		return distance;
+	}
+
+	public int getMinmag(){
+		return minmag;
+	}
+
+	public void setDistance(int dist){
+		this.distance = dist;
+	}
+
+	public void setMinmag(int min){
+		this.minmag = min;
+	}
+}
+
+
+
